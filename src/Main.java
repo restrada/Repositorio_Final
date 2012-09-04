9d1b8a73255d8a9ea620f5bc9debcf367c2239cc
Rodrigo Espíndola Estrada <spajx@hotmail.com>
2012-09-04 11:47:51 -0500

    Se sube el Proyecto

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		double a = 5.4;
+		double b = 7.5;
+		double c = a * b;
+		System.out.println("Hello world!!!");
+		System.out.println("El Resultado de " + a + " * " + b + " = " + c);
+	}
+}
